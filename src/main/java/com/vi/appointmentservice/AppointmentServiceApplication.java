5e0d3f6a470f50c0bbc25ddaafce953ace909003
PhilippFruh <philipp.fruh@pm.me>
2022-06-21 18:02:43 +0200

    feat: add initial spring boot code structure
    Signed-off-by: PhilippFruh <philipp.fruh@pm.me>

---
+++ src/main/java/com/vi/appointmentservice/AppointmentServiceApplication.java
@@ -0,0 +1,21 @@
+package com.vi.appointmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starter class for the application.
+ */
+@SpringBootApplication
+public class AppointmentServiceApplication {
+
+  /**
+   * Global application entry point.
+   *
+   * @param args possible provided args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(com.vi.appointmentservice.AppointmentServiceApplication.class, args);
+  }
+
+}
