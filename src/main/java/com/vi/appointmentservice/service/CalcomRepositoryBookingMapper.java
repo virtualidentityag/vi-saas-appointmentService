fd85ec045478947eaefe19b142e858eef8cf9481
aalicic <adnan@leads.ba>
2022-07-17 19:08:51 +0200

    feat/publishing messages on reschedule

--- src/main/java/com/vi/appointmentservice/service/CalcomRepositoryBookingMapper.java
+++ src/main/java/com/vi/appointmentservice/service/CalcomRepositoryBookingMapper.java
@@ -3,6 +3,8 @@ package com.vi.appointmentservice.service;
 import com.vi.appointmentservice.api.model.CalcomBooking;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.jdbc.core.RowMapper;
 
 public class CalcomRepositoryBookingMapper implements RowMapper<CalcomBooking> {
@@ -12,11 +14,18 @@ public class CalcomRepositoryBookingMapper implements RowMapper<CalcomBooking> {
     CalcomBooking booking = new CalcomBooking();
     booking.setId(rs.getInt("id"));
     booking.setTitle(rs.getString("title"));
-    booking.setStartTime(rs.getString("startTime"));
-    booking.setEndTime(rs.getString("endTime"));
+    booking.setStartTime(formatDate(rs.getString("startTime")));
+    booking.setEndTime(formatDate(rs.getString("endTime")));
     booking.setUserId(rs.getInt("userId"));
     booking.setEventTypeId(rs.getInt("eventTypeId"));
     booking.setUid(rs.getString("uid"));
+    booking.setDescription(rs.getString("description"));
     return booking;
   }
+
+  private String formatDate(String dateTime){
+    return LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).plusHours(4).toString();
+  }
 }
+
+
