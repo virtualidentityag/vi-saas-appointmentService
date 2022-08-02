71e4261da30cccea6a695873cf33e4a28d29daf7
PhilippFruh <philipp.fruh@pm.me>
2022-08-02 14:08:34 +0200

    fix: nameParam is SQL statement
    Signed-off-by: PhilippFruh <philipp.fruh@pm.me>

--- src/main/java/com/vi/appointmentservice/repository/UserRepository.java
+++ src/main/java/com/vi/appointmentservice/repository/UserRepository.java
@@ -17,7 +17,7 @@ public class UserRepository {
     String name = user.getName();
     Boolean isAway = user.getAway();
     String UPDATE_USER_QUERY = "update \"users\" set \"name\" = $nameParam, \"away\" = $awayParam where \"userId\" = " + userId;
-    UPDATE_USER_QUERY = UPDATE_USER_QUERY.replace("$awayParam", name.toString())
+    UPDATE_USER_QUERY = UPDATE_USER_QUERY.replace("$nameParam", name.toString())
         .replace("$awayParam", isAway.toString());
     jdbcTemplate.update(UPDATE_USER_QUERY);
   }
