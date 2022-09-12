27523853f0dd4be153aa6690dc74863241bea4dc
PhilippFruh <philipp.fruh@pm.me>
2022-09-12 14:25:49 +0200

    feat: add statistics service
    Signed-off-by: PhilippFruh <philipp.fruh@pm.me>

---
+++ src/main/java/com/vi/appointmentservice/api/service/statistics/event/StatisticsEvent.java
@@ -0,0 +1,22 @@
+package com.vi.appointmentservice.api.service.statistics.event;
+
+
+import java.util.Optional;
+
+/** Interface for statistics event. */
+public interface StatisticsEvent {
+
+  /**
+   * Provides the payload for the statistics event message.
+   *
+   * @return the payload as {@link Optional}
+   */
+  Optional<String> getPayload();
+
+  /**
+   * The event type of the statistics event.
+   *
+   * @return the {@link EventType}
+   */
+  com.vi.appointmentservice.statisticsservice.generated.web.model.EventType getEventType();
+}
